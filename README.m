5c31b828198459e53cbfcacade3d55d1de83d7cb
Fellicks <127662466+Fellicks@users.noreply.github.com>
2023-09-11 12:55:45 +0300

    Create README.m

---
+++ README.m
@@ -0,0 +1,10 @@
+-👋 Hi I’m @Fellicks
+- 👀 I’m interested in python automation and web development...
+- 🌱 I’m currently learning back end web development by django...
+- 💞️ I’m looking to collaborate on full stack web development and python automation...
+- 📫 How to reach me through fellokay98@gmail.com
+
+<!---
+Fellicks/Fellicks is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
